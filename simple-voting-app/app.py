740a4974b897dea2c28055ff5c22fff89a022787
Mallemputisaiteja <112643626+Mallemputisaiteja@users.noreply.github.com>
2025-04-16 12:10:54 +0530

    Add files via upload

---
+++ simple-voting-app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+votes = {"Cats": 0, "Dogs": 0}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    global votes
+    if request.method == "POST":
+        if request.form.get("action") == "clear":
+            votes = {"Cats": 0, "Dogs": 0}
+        else:
+            vote = request.form.get("animal")
+            if vote in votes:
+                votes[vote] += 1
+    return render_template("index.html", votes=votes)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
