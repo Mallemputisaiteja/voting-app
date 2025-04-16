06cde2767a15eaa219850d20ffcc0ca756e4ff28
Mallemputisaiteja <112643626+Mallemputisaiteja@users.noreply.github.com>
2025-04-16 14:14:33 +0530

    Update app.py

--- simple-voting-app/app.py
+++ simple-voting-app/app.py
@@ -17,4 +17,5 @@ def index():
     return render_template("index.html", votes=votes)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
